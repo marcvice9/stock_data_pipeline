22eb40895b8c61b87370598d7716048756ca7222
marcvice9 <marcvicente9@gmail.com>
2025-04-02 19:00:32 +0200

    feat: add main to centralize pipeline steps

---
+++ src/main.py
@@ -0,0 +1,45 @@
+from src.ingestion_stock import fetch_stock_data
+from src.db.db_operations import create_db_engine, init_db, insert_raw_data
+from src.config.constants import TICKERS
+import pandas as pd
+import time
+
+def main():
+
+    all_tickers = []
+
+    for ticker in TICKERS:
+        print(f"Fetching data for {ticker}...")
+        records=fetch_stock_data(ticker)
+        all_tickers.extend(records)  # Collect data for all tickers
+        time.sleep(12)  # Sleep for 12 seconds to avoid hitting the API rate limit
+
+    if all_tickers:
+        
+        df = pd.DataFrame(all_tickers)
+
+        # Convert 'Date' to datetime format
+        df['Date'] = pd.to_datetime(df['Date'])
+
+        # Set 'Date' as the index
+        df.set_index('Date', inplace=True)
+
+        # Display all columns in the DataFrame
+        pd.set_option('display.max_columns', None)
+        
+        # Print the resulting DataFrame
+        print(df)
+
+    else:
+        print("No data to display.")
+    
+    # Initialize the database and create tables
+    try:
+        db_engine = init_db()
+        insert_raw_data(db_engine, df, table_name='raw_stock')
+    except Exception as e:
+        print("Database connection failed. Data not inserted.")
+
+
+if __name__ == "__main__":
+    main()
