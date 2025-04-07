d83a5505b955695c67f04271a0f10f8946bf2bcd
marcvice9 <marcvicente9@gmail.com>
2025-04-07 15:57:57 +0200

    chore: modify default db env variables to default to local postgres

--- src/db/db_operations.py
+++ src/db/db_operations.py
@@ -8,11 +8,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Database connection string
-DB_USER = os.getenv("DB_USER", "stockuser")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "stockpassword")
-DB_HOST = os.getenv("DB_HOST", "postgres")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "stock_db")
+DB_NAME = os.getenv("DB_NAME", "postgres")
 
 
 def create_db_engine():
