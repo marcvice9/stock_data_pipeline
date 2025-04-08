dae1194e002c43addeb07a8a983a4878d7bc5163
marcvice9 <marcvicente9@gmail.com>
2025-04-08 09:45:56 +0200

    chore: update default database name to stock_db

--- src/db/db_operations.py
+++ src/db/db_operations.py
@@ -23,7 +23,7 @@ DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "postgres")
+DB_NAME = os.getenv("DB_NAME", "stock_db")
 
 
 def create_db_engine():
